9cc155e44f0f87cd7047e7a57887f7313a68ddc8
taha uygun <tahauygun@gmail.com>
2019-05-15 21:33:37 -0700

    adding dynamic cards

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  },
+  {
+    id: 2,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  },
+  {
+    id: 3,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  },
+  {
+    id: 4,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  },
+  {
+    id: 5,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  },
+  {
+    id: 6,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  },
+  {
+    id: 7,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email:'taco@taco.com'
+  }
+]
